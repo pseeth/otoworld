223b88ee993fa8ccd8121dbeba8850d8628d6db4
Omkar Ranadive <Omkar Ranadive>
2020-01-14 22:57:48 -0600

    Quick and dirty implementation of function which returns convolved signal

---
+++ code/code/basicRoom.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from scipy.signal import fftconvolve
+import pyroomacoustics as pra
+
+
+def init_env(direct_sound, agent_loc, sound_loc, room_config):
+    '''
+
+    :param direct_sound: The path to audio file
+    :param agent_loc: Agent/microphone location (numpy array)
+    :param sound_loc: Location of audio source (list)
+    :param room_config: List of room dimensions, 2d or 3d
+    :return: Convolved sound
+    '''
+
+    # Load the audio file
+    rate, audio = wavfile.read(direct_sound)
+    print(audio.shape)
+    # As the test signal is recorded on stereo microphone, it is 2d
+    # Choose one of the sides
+    audio = audio[:, 0]
+    # Create a room using shoe box function
+    room = pra.ShoeBox(room_config)
+    room.add_source(sound_loc, signal=audio)
+    # Create the microphone array
+    mic = pra.MicrophoneArray(agent_loc, room.fs)
+    room.add_microphone_array(mic)
+
+    # Impulse responses
+    room.compute_rir()
+    # plt.plot(room.rir[0][0])
+    # plt.show()
+
+    # Convolve the sounds
+    room.simulate()
+    # print(room.mic_array.signals.shape)
+    data = room.mic_array.signals[0, :]
+
+    # Data needs to be in integer before being saved
+    scaled = np.int16(data / np.max(np.abs(data)) * 32767)
+
+    # Save the convolved sound
+    wavfile.write('../sounds/ConvolvedSounds/convolved_sound.wav', rate, data=scaled)
+
+    # plot signal at microphone 0
+    plt.plot(room.mic_array.signals[0, :])
+    plt.show()
+
+    return room.mic_array.signals[0:, ]
+
+
+if __name__ == '__main__':
+    path = '../sounds/demoaudio.wav'
+    room_config = [10, 10]
+    source_loc = [5, 5]
+    agent_loc = np.array([2, 3]).reshape(-1, 1)
+
+    init_env(path, agent_loc, source_loc, room_config)
+
---
+++ code/code/temp.py
@@ -0,0 +1,27 @@
+# import torch
+#
+# print(torch.cuda.current_device())
+# print(torch.cuda.get_device_name())
+# print(torch.cuda.is_available())
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pyroomacoustics as pra
+
+# Create a 4 by 6 metres shoe box room
+room = pra.ShoeBox([4,6])
+
+# Add a source somewhere in the room
+room.add_source([2.5, 4.5])
+
+# Create a linear array beamformer with 4 microphones
+# with angle 0 degrees and inter mic distance 10 cm
+R = pra.linear_2D_array([2, 1.5], 4, 0, 0.1)
+room.add_microphone_array(pra.Beamformer(R, room.fs))
+
+# Now compute the delay and sum weights for the beamformer
+room.mic_array.rake_delay_and_sum_weights(room.sources[0][:1])
+
+# plot the room and resulting beamformer
+room.plot(freq=[1000, 2000, 4000, 8000], img_order=0)
+plt.show()
