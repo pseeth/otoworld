af2c399435e9689e0ed7d679ba1c99c445b213a2
davidterpay <david.terpay@gmail.com>
2020-01-23 14:41:20 -0600

    Adding librosa and computing mean of stereos

--- code/src/basic_room.py
+++ code/src/basic_room.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.io import wavfile
+import librosa
 from scipy.signal import fftconvolve
 from pyroomacoustics import MicrophoneArray, ShoeBox
 
-
+# NOTE: the resample rate is defaulted to 8Khz for computational reasons
+RESAMPLE_RATE = 8000
 def init_env(direct_sound, agent_loc, sound_loc, room_config):
     '''
 
@@ -20,18 +21,14 @@ def init_env(direct_sound, agent_loc, sound_loc, room_config):
 
     # Load the audio file(s), store data in parallel arrays
     audio = []
-    rates = []
     for idx, audio_file in enumerate(direct_sound):
-        r, a = wavfile.read(audio_file)
-        print("Rate: ", r)
-
-        # store
-        rates.append(r)
+        # Audio will be automatically resampled to the given rate (default sr=8000).
+        a, __ = librosa.load(audio_file, sr=RESAMPLE_RATE)
 
         # If sound is recorded on stereo microphone, it is 2d
-        # Choose one of the sides
+        # take the mean of the two stereos
         if len(a.shape) > 1:
-            a = a[:, 0]
+            a = np.mean(a, axis = 0)
 
         audio.append(a)
 
@@ -51,12 +48,9 @@ def init_env(direct_sound, agent_loc, sound_loc, room_config):
     # print(room.mic_array.signals.shape)
     data = room.mic_array.signals[0, :]
 
-    # Data needs to be in integer before being saved
-    scaled = np.int16(data / np.max(np.abs(data)) * 32767)
-
     # Save the convolved sound
-    # NOTE: rates[0] may not be ideal, may need to make more robust later
-    wavfile.write('../sounds/convolved_sounds/convolved_sound.wav', rates[0], data=scaled)
+    librosa.output.write_wav(
+        '../sounds/convolved_sounds/convolved_sound.wav', data, RESAMPLE_RATE, norm=True)
 
     # plot signal at microphone 0
     plt.plot(room.mic_array.signals[0, :])
