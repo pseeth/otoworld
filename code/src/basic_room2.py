0053d357fa183f36f3b5f6425bed70064cbe5d30
Grant Gasser <glgasser@gmail.com>
2020-01-28 21:38:03 -0600

    Choose random source files in basic_room2.py

--- code/src/basic_room2.py
+++ code/src/basic_room2.py
@@ -2,12 +2,15 @@ import gym
 import audio_room
 import numpy as np
 import rl_agent
+import os
+
+DIR_MALE = '../sounds/dry_recordings/dev/051_subset/'
+DIR_FEMALE = '../sounds/dry_recordings/dev/050_subset/'
 
 
 def run_random_agent():
 	# paths of audio files
-	paths = ['../sounds/dry_recordings/dev/050_subset/050a050a.wav',
-			 '../sounds/dry_recordings/dev/051_subset/051a050b.wav']
+	paths = choose_random_files()
 
 	# dimensions of room
 	room_config = [10, 10]
@@ -29,8 +32,7 @@ def run_random_agent():
 
 def run_perfect_agent():
 	# paths of audio files
-	paths = ['../sounds/dry_recordings/dev/050_subset/050a050a.wav',
-			 '../sounds/dry_recordings/dev/051_subset/051a050b.wav']
+	paths = choose_random_files()
 
 	# dimensions of room
 	room_config = [30, 30]
@@ -39,7 +41,7 @@ def run_perfect_agent():
 	source_loc = [[0, 0], [14, 14]]  # Keep the sources far away for easy testing
 
 	# location of agent (or microphone in this case)
-	agent_loc = np.array([5, 5])
+	agent_loc = np.array([11, 11])
 
 	# Set up the gym environment
 	env = gym.make('audio-room-v0', room_config=room_config, agent_loc=agent_loc)
@@ -50,6 +52,28 @@ def run_perfect_agent():
 	target_loc = env.target
 	agent = rl_agent.PerfectAgent(target_loc=target_loc, agent_loc=agent_loc)
 	agent.fit(env)
+
+
+def choose_random_files():
+	paths = []
+
+	# pick random files from each directory
+	for dir in [DIR_MALE, DIR_FEMALE]:
+		files = os.listdir(dir)
+
+		# remove non-wav files
+		indexes = []
+		for i, file in enumerate(files):
+			if '.wav' not in file:
+				indexes.append(i)
+		for i in indexes:
+			del files[i]
+
+		# pick random file
+		index = np.random.randint(len(files), size=1)[0]
+		paths.append(os.path.join(dir, files[index]))
+
+	return paths
 
 
 if __name__ == '__main__':
